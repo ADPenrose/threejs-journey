5adbd081ae9d34be0a8b8abfa63ba228d4e77950
Arturo Avila <mysticmetromx@gmail.com>
2024-02-11 15:14:50 -0600

    Completed materials lesson

--- 01-Basics/1.8-materials/src/script.js
+++ 01-Basics/1.8-materials/src/script.js
@@ -1,9 +1,15 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import GUI from 'lil-gui';
+import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js';
+
+// Cretaing a gui instance.
+const gui = new GUI();
 
 /**
  * Base
  */
+
 // Canvas
 const canvas = document.querySelector('canvas.webgl');
 
@@ -57,11 +63,84 @@ matcapTexture.colorSpace = THREE.SRGBColorSpace;
 // const material = new THREE.MeshPhongMaterial();
 
 // MeshToonMaterial
-const material = new THREE.MeshToonMaterial();
-gradientTexture.minFilter = THREE.NearestFilter;
-gradientTexture.magFilter = THREE.NearestFilter;
-gradientTexture.generateMipmaps = false;
-material.gradientMap = gradientTexture;
+// const material = new THREE.MeshToonMaterial();
+// gradientTexture.minFilter = THREE.NearestFilter;
+// gradientTexture.magFilter = THREE.NearestFilter;
+// gradientTexture.generateMipmaps = false;
+// material.gradientMap = gradientTexture;
+
+// MeshStandardMaterial
+// const material = new THREE.MeshStandardMaterial();
+// material.metalness = 1;
+// material.roughness = 1;
+// gui.add(material, 'metalness').min(0).max(1).step(0.0001);
+// gui.add(material, 'roughness').min(0).max(1).step(0.0001);
+// material.map = doorColorTexture;
+// material.aoMap = doorAmbientOcclusionTexture;
+// material.aoMapIntensity = 1;
+// material.displacementMap = doorHeightTexture;
+// material.displacementScale = 0.1;
+// material.metalnessMap = doorMetalnessTexture;
+// material.roughnessMap = doorRoughnessTexture;
+// material.normalMap = doorNormalTexture;
+// material.normalScale.set(0.5, 0.5);
+// material.alphaMap = doorAlphaTexture;
+// material.transparent = true;
+
+// MeshPhysicalMaterial
+const material = new THREE.MeshPhysicalMaterial();
+// These are at 0 just so that we can see the effect of the transmission on a
+// clear material.
+material.metalness = 0;
+material.roughness = 0;
+gui.add(material, 'metalness').min(0).max(1).step(0.0001);
+gui.add(material, 'roughness').min(0).max(1).step(0.0001);
+// material.map = doorColorTexture;
+// material.aoMap = doorAmbientOcclusionTexture;
+// material.aoMapIntensity = 1;
+// material.displacementMap = doorHeightTexture;
+// material.displacementScale = 0.1;
+// material.metalnessMap = doorMetalnessTexture;
+// material.roughnessMap = doorRoughnessTexture;
+// material.normalMap = doorNormalTexture;
+// material.normalScale.set(0.5, 0.5);
+// material.alphaMap = doorAlphaTexture;
+// material.transparent = true;
+
+// Clearcoat
+// material.clearcoat = 1;
+// material.clearcoatRoughness = 0;
+// gui.add(material, 'clearcoat').min(0).max(1).step(0.0001);
+// gui.add(material, 'clearcoatRoughness').min(0).max(1).step(0.0001);
+
+// Sheen
+// material.sheen = 1;
+// material.sheenRoughness = 0.25;
+// material.sheenColor.set(1, 1, 1);
+// gui.add(material, 'sheen').min(0).max(1).step(0.0001);
+// gui.add(material, 'sheenRoughness').min(0).max(1).step(0.0001);
+// gui.addColor(material, 'sheenColor');
+
+// Iridescence
+// material.iridescence = 1;
+// material.iridescenceIOR = 1;
+// material.iridescenceThicknessRange[(100, 800)];
+// gui.add(material, 'iridescence').min(0).max(1).step(0.0001);
+// // All of the materials IRL have an IOR that ranges from 1 to 2.333 aprox.
+// gui.add(material, 'iridescenceIOR').min(1).max(2.333).step(0.0001);
+// gui.add(material.iridescenceThicknessRange, 0).min(0).max(1000).step(1);
+// gui.add(material.iridescenceThicknessRange, 1).min(0).max(1000).step(1);
+
+// Transmission
+material.transmission = 1;
+material.ior = 1.5;
+// This is a fixed value and the actual thickness of the object is not taken
+// into account.
+material.thickness = 0.5;
+gui.add(material, 'transmission').min(0).max(1).step(0.0001);
+// The IOR range will not be realistic, but it will be enough to see the effect.
+gui.add(material, 'ior').min(1).max(10).step(0.0001);
+gui.add(material, 'thickness').min(0).max(1).step(0.0001);
 
 // First mesh: Sphere
 const sphereGeomerty = new THREE.SphereGeometry(0.5, 64, 64);
@@ -69,27 +148,37 @@ const sphereMaterial = material;
 const sphereMesh = new THREE.Mesh(sphereGeomerty, sphereMaterial);
 
 // Second mesh: Plane
-const planeGeometry = new THREE.PlaneGeometry(1, 1);
+const planeGeometry = new THREE.PlaneGeometry(1, 1, 100, 100);
 const planeMaterial = material;
 const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
 planeMesh.position.x = -2;
 
 // Third mesh: Torus
-const torusGeometry = new THREE.TorusGeometry(0.5, 0.2);
+const torusGeometry = new THREE.TorusGeometry(0.5, 0.2, 64, 128);
 const torusMaterial = material;
 const torusMesh = new THREE.Mesh(torusGeometry, torusMaterial);
 torusMesh.position.x = 2;
 scene.add(sphereMesh, planeMesh, torusMesh);
 
 // Lights. Parameters: color, intensity
-const ambientLight = new THREE.AmbientLight(0xffffff, 1);
-scene.add(ambientLight);
+// const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+// scene.add(ambientLight);
 
-const pointLight = new THREE.PointLight(0xffffff, 30);
-pointLight.position.x = 2;
-pointLight.position.y = 3;
-pointLight.position.z = 4;
-scene.add(pointLight);
+// const pointLight = new THREE.PointLight(0xffffff, 30);
+// pointLight.position.x = 2;
+// pointLight.position.y = 3;
+// pointLight.position.z = 4;
+// scene.add(pointLight);
+
+// Environment map
+const rgbeLoader = new RGBELoader();
+rgbeLoader.load('textures/environmentMap/2k.hdr', (environmentMap) => {
+	environmentMap.mapping = THREE.EquirectangularReflectionMapping;
+	// Adds the background to the scene.
+	scene.background = environmentMap;
+	// This allows the background to contribute to the lighting of the scene.
+	scene.environment = environmentMap;
+});
 
 /**
  * Sizes
